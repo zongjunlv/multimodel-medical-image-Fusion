27dbaef79c6ea09ef508388fb8ecaed8af015451
zongjunlv <zongjunlv@example.com>
2026-01-05 07:28:57 +0000

    ignore 3DSS-Mamba

--- MedMamba/data/medical_dataset.py
+++ MedMamba/data/medical_dataset.py
@@ -1,56 +1,41 @@
 import os
-
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset
-import nibabel as nib
-
-
 import numpy as np
 import pandas as pd
 
-from monai.transforms import Compose, ScaleIntensity, EnsureChannelFirst, Resize, RandRotate90, CenterSpatialCrop
+import torch
+import torch.nn as nn
+import nibabel as nib
+from torch.utils import data
+from torch.utils.data import Dataset
+from monai.transforms import (
+    Compose, RandRotate90, RandFlip, RandGaussianNoise)
+
 
 
 class Medical_Dataset(Dataset):
-    def __init__(self,  mode, target_size = (96,96,96)):
-        if mode == 'train':
-            self.csv_path = '/data02/workspace/LZJ_SPACE/dataset/ABUS_Classification/330_512_512/330_512_512_train.csv'
-            self.transforms = Compose([
-                EnsureChannelFirst(channel_dim='no_channel'),
-                ScaleIntensity(),
-                # CenterSpatialCrop(roi_size=(256, 256, 256)),
-                Resize(target_size),
-                RandRotate90()
-            ])
-        else:
-            self.csv_path = '/data02/workspace/LZJ_SPACE/dataset/ABUS_Classification/330_512_512/330_512_512_val.csv'
-            self.transforms = Compose([
-                EnsureChannelFirst(channel_dim='no_channel'),
-                ScaleIntensity(),
-                # CenterSpatialCrop(roi_size=(256, 256, 256)),
-                Resize(target_size),
-            ])
-        self.data = pd.read_csv(self.csv_path)
+    def __init__(self,  mode, csv_path):
+        self.mode = mode
+        self.data = pd.read_csv(csv_path)
+        self.train_aug = Compose([
+            RandFlip(prob=0.5, spatial_axis=0),
+            RandFlip(prob=0.5, spatial_axis=1),
+            RandFlip(prob=0.5, spatial_axis=2),
+            RandRotate90(prob=0.5, spatial_axes=(0, 1)),
+            RandRotate90(prob=0.5, spatial_axes=(0, 2)),
+            RandGaussianNoise(prob=0.2, mean=0.0, std=0.01)
+        ])
         
-        self.class_map = {0:'benign', 1:'healthy', 2:'malignant'}
     
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        base_dir = "/data02/workspace/LZJ_SPACE"  # 或者 Path(self.csv_path).parent.parent 等动态求
-        image_path = os.path.join(base_dir, self.data['file_path'][idx])
-        image = nib.load(image_path).get_fdata(dtype=np.float32)
 
         row = self.data.iloc[idx]
-        label_id = int(row["label"])
-        # label = self.class_map[label_id]
-        label = torch.tensor(label_id, dtype=torch.long)
+        img = np.load(row["npy_path"])
+        img = torch.from_numpy(img.copy()).float()
+        if self.mode == 'train':
+            img = self.train_aug(img)
+        label = torch.as_tensor(int(row["label"]), dtype=torch.long)
 
-
-        image = self.transforms(image)
-
-        return image, label
-
-
+        return img, label
--- MedMamba/train.py
+++ MedMamba/train.py
@@ -1,19 +1,23 @@
 import os
-from matplotlib.style import available
+from sched import scheduler
 import time
+import math
 import random
+from matplotlib.style import available
+
 import numpy as np
+import pandas as pd
 import swanlab
-import random
 from datetime import timedelta
-from timm.models.layers import weight_init
 import torch
+from timm.models.layers import weight_init
 from torch.cuda import Device
 from torch.optim import optimizer
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 import torch.nn as nn
 
 
+from models.medmamba3d import VSSM3D
 from trainer import Trainer
 from utils import compute_all_metrics
 from data.medical_dataset import Medical_Dataset
@@ -29,6 +33,11 @@ def set_seed(seed):
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
 
+def warmup_cosine(warmup_epochs, num_epochs, epoch):
+    if epoch < warmup_epochs:
+        return float(epoch + 1) / warmup_epochs  # 逐步升到基础 lr
+    progress = (epoch - warmup_epochs) / (num_epochs - warmup_epochs)
+    return 0.5 * (1.0 + math.cos(math.pi * progress))  # cosine 下降到 0
 
 def main():
 
@@ -39,41 +48,87 @@ def main():
     start_time = time.time()
     seed = 3407
     set_seed(seed)
-    
-    
 
-    train_dataset = Medical_Dataset(mode='train')
-    val_dataset = Medical_Dataset(mode='val')
+    train_path = '/data02/workspace/LZJ_SPACE/MedMamba/dataset/ABUS_Classification/ABUS_NEW/split_train_7_1_2.csv'
+    val_path = '/data02/workspace/LZJ_SPACE/MedMamba/dataset/ABUS_Classification/ABUS_NEW/split_val_7_1_2.csv'
+    test_path = '/data02/workspace/LZJ_SPACE/MedMamba/dataset/ABUS_Classification/ABUS_NEW/split_test_7_1_2.csv'
+
+    train_dataset = Medical_Dataset(mode='train', csv_path= train_path)
+    val_dataset = Medical_Dataset(mode='val', csv_path= val_path)
+    test_dataset = Medical_Dataset(mode='test', csv_path= test_path)
+    
 
     batch_size = 8
     lr = 1e-5
     weight_decay = 1e-2
 
-    train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True, pin_memory=True, drop_last=True,
-                                        num_workers=12,persistent_workers=True, prefetch_factor=8)
-    val_dataloader = DataLoader(val_dataset, batch_size, shuffle=False, pin_memory=True,
-                                        num_workers=12,persistent_workers=True, prefetch_factor=8)
+    # 读取训练集标签统计（按文件，标签 0/1/2）
+    train_df = pd.read_csv(train_path)
+    counts = train_df['label'].value_counts().reindex([0,1,2], fill_value=1).astype(float)
+    # 采样权重：使用 sqrt(1/freq) 以避免过度倾向少数类
+    weight_map = (counts.max() / counts) ** 0.5
+    weight_map = weight_map / weight_map.sum()
+    train_labels = train_df['label'].tolist()
+    sample_weights = [weight_map[l] for l in train_labels]
+    sampler = WeightedRandomSampler(sample_weights, num_samples=len(sample_weights), replacement=True)
 
+
+    train_dataloader = DataLoader(train_dataset, 
+                                  batch_size, 
+                                #   shuffle=True, 
+                                  sampler=sampler,
+                                  pin_memory=True, 
+                                  drop_last=True,                                        num_workers=12,
+                                  persistent_workers=True, 
+                                  prefetch_factor=8)
+    
+    val_dataloader = DataLoader(val_dataset, 
+                                batch_size, 
+                                shuffle=False, 
+                                pin_memory=True,                                        num_workers=12,
+                                persistent_workers=True, 
+                                prefetch_factor=8)
+    
+    test_dataloader = DataLoader(test_dataset, 
+                                 batch_size, 
+                                 shuffle=False, 
+                                 pin_memory=True,
+                                 num_workers=12,
+                                 persistent_workers=True, 
+                                 prefetch_factor=8)
     device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
-    model = Model()
+    # model = Model()
+
+    model = VSSM3D()
     model.to(device)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay )
-    criterion = nn.CrossEntropyLoss()
+
+    
+    # 反比权重，防止过大可以再做归一化
+    weights_cls = (counts.max() / counts) ** 0.5   # 开平方降温
+    weights_cls = weights_cls / weights_cls.mean()
+    class_weights = torch.tensor(weights_cls.values, dtype=torch.float32, device=device)
+    criterion = nn.CrossEntropyLoss(weight=class_weights)
 
     print("\nCONFIGURATION")
     print(f"  {'Batch size':18}: {batch_size}")
     print(f"  {'Learning rate':18}: {lr}")
     print(f"  {'Device':18}: {device}")
-    print(f"  {'Mixed Precision':18}: {'Enabled' if device.type == 'cuda' else 'Disabled (CPU)'}")
+    print(f"  {'Mixed Precision':18}: {'Unabled' if device.type == 'cuda' else 'Disabled (CPU)'}")
 
     print("\nPREPARING DATA...")
 
 
     trainer = Trainer(model, optimizer, criterion, device)                                      
 
-    num_epochs = 100
+    warmup_epochs = 5
+    num_epochs = 500
+    scheduler = torch.optim.lr_scheduler.LambdaLR(
+        optimizer,
+        lr_lambda=lambda epoch: warmup_cosine(warmup_epochs, num_epochs, epoch)
+    )
     best_val_loss = float('inf')
     early_stop = 0
     patience = 10
@@ -100,6 +155,7 @@ def main():
         
         train_loss = trainer.train(train_dataloader)
         val_loss = trainer.validate(val_dataloader)
+        scheduler.step()
 
         epoch_time = time.time() - epoch_start
         print(f"  Train loss: {train_loss:.4f}   Val loss: {val_loss:.4f}   Time: {str(timedelta(seconds=int(epoch_time)))}")
@@ -114,15 +170,15 @@ def main():
         else:
             early_stop += 1
             print(f"  Early stopping count: {early_stop}/{patience}")
-            if early_stop == patience:
-                print(f"  Early stopping triggered at epoch {epoch + 1}.")
-                print(f"  Best model saved as: best_model.pth")
-                break
+            # if early_stop == patience:
+            #     print(f"  Early stopping triggered at epoch {epoch + 1}.")
+            #     print(f"  Best model saved as: best_model.pth")
+            #     break
         
         if epoch % 5 == 0:
-            accuracy, auc, sensitivity, specificity, f1, mcc = evaluate_model(model, val_dataloader, device, verbose=True)
+            accuracy, auc, sensitivity, specificity, f1, mcc = evaluate_model(model, test_dataloader, device, verbose=True)
         else:
-            accuracy, auc, sensitivity, specificity, f1, mcc = evaluate_model(model, val_dataloader, device, verbose=False)
+            accuracy, auc, sensitivity, specificity, f1, mcc = evaluate_model(model, test_dataloader, device, verbose=False)
         swanlab.log({"train_loss":train_loss,
                     "val_loss": val_loss, 
                     "acc":accuracy,
